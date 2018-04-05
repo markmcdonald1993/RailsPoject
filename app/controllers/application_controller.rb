f244ef60afaf0b4b1b074d98a37a2168529cb951
x14503387 <markmcd1993@gmail.com>
2018-04-05 11:06:47 +0000

    profile management done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
    protect_from_forgery with: :exception
    
+   before_action :configure_permitted_parameters, if: :devise_controller?
+   
    protected 
    
    def configure_permitted_parameters
-       devise_parameters_sanitizer.for(:sign_up){|u| u.permit(:firstname, :lastname, :email, :password, :remember_me)}
-       devise_parameters_sanitizer.for(:sign_in){|u| u.permit(:firstname, :lastname, :email, :password, :remember_me)}
-       devise_parameters_sanitizer.for(:account_update){|u| u.permit(:firstname, :lastname, :email, :password, :remember_me)}
+       devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :email, :password, :remember_me])
+       devise_parameter_sanitizer.permit(:sign_in, keys: [:firstname, :lastname, :email, :password, :remember_me])
+       devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :email, :password, :remember_me])
    end 
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,7 @@
 class Patient < ApplicationRecord
+    def self.search(search)
+        where("lastname LIKE ?", "%#{search}%")
+    end
+    
+    belongs_to :user
 end
